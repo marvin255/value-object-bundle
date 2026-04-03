302deac21eacef3f99e22cf463ffe15447d663c9
marvin255 <postbox433@gmail.com>
2026-04-04 00:39:13 +0200

    Fix phpunit notices

--- tests/BaseCase.php
+++ tests/BaseCase.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Marvin255\ValueObjectBundle\Tests;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
-use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\MockObject\Stub;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -18,10 +18,10 @@ abstract class BaseCase extends TestCase
     /**
      * Create mock for AbstractPlatform.
      *
-     * @return MockObject&AbstractPlatform
+     * @return Stub&AbstractPlatform
      */
     protected function createAbstarctPlatformMock(): AbstractPlatform
     {
-        return $this->getMockBuilder(AbstractPlatform::class)->getMock();
+        return $this->createStub(AbstractPlatform::class);
     }
 }
--- tests/Type/IntPositiveValueObjectTypeTest.php
+++ tests/Type/IntPositiveValueObjectTypeTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Marvin255\ValueObjectBundle\Tests\Type;
 
 use Doctrine\DBAL\ParameterType;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Marvin255\ValueObject\IntPositiveValueObject;
 use Marvin255\ValueObjectBundle\Tests\BaseCase;
 use Marvin255\ValueObjectBundle\Type\IntPositiveValueObjectType;
@@ -18,9 +19,9 @@ final class IntPositiveValueObjectTypeTest extends BaseCase
     public function testGetSQLDeclaration(): void
     {
         $type = new IntPositiveValueObjectType();
-        $platform = $this->createAbstarctPlatformMock();
         $column = ['name' => 'test_column'];
 
+        $platform = $this->getMockBuilder(AbstractPlatform::class)->getMock();
         $platform->expects($this->once())
             ->method('getIntegerTypeDeclarationSQL')
             ->with($column)
